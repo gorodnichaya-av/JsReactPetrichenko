d7c6f436f5aab3ea0b8f55a8cc9e6c832b6e423f
Anastasiia <monkeyPink1313@outlook.com>
2023-03-15 10:18:00 +0200

    practical #1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const numberOfFIlms = +prompt('How many films did you see?', ''),
+      personalMovieDB = {
+        count: numberOfFIlms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+      },
+      filmName = prompt('Last film?', ''),
+      filmRaiting = prompt('Raiting?', ''),
+      filmName2 = prompt('Last film?', ''),
+      filmRaiting2 = prompt('Raiting?', '');
+
+personalMovieDB.movies[filmName] = filmRaiting;
+personalMovieDB.movies[filmName2] = filmRaiting2;
+
+console.log(personalMovieDB);
+
