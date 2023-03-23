27abe2a708391cb456f74d497e7ad7b7fe270b04
Anastasiia <monkeyPink1313@outlook.com>
2023-03-23 10:07:03 +0200

    small task script

---
+++ js/scriptSmallTasks.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// let x = 5; console.log(x++); // 5
+
+// console.log([] + false - null + true); // NaN ([] = '' (string));
+
+// let y = 1; let x = y = 2; console.log(x); // 2
+
+// console.log([] + 1 + 2); // 12 ([] = '' (string))
+
+// console.log('1'[0]); //1 (index [0] in sctring(as array) = 1)
+
+// console.log(2 && 1 && null && 0 && undefined); // null 
+// && завжди зупиняється на false
+// || завжди зупиняється на true
+
+// console.log(!!(1 && 2) === (1 && 2)); //false
+
+// console.log(null || 2&&3 || 4); // 3 Дивись таблицю пріоритетів операторів
+
+// const a = [1,2,3],
+//       b = [1,2,3];
+// console.log(a === b); // false
+
+// console.log( +'Infinity' ); // Infinity
+
+// console.log('pear' > 'apple'); // true P has bigger index then A
+
+// console.log(0 || '' || 2 || undefined || true || false); // 2
+
+
