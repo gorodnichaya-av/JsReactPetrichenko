0431c71b0a0229533f4481eed8f1c258576173da
Anastasiia <monkeyPink1313@outlook.com>
2023-03-29 15:34:17 +0300

    lesson 042 043: modal

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -93,4 +93,52 @@ window.addEventListener('DOMContentLoaded', () => {
 
     setClock('.timer', deadline);
 
+    // MODAL
+    const modalBtn = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('.modal__close');
+
+    modalBtn.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    }); 
+
+    function openModal() {
+        // modal.classList.toggle('show');
+        modal.classList.add('show');
+        document.body.style.overflow = 'hidden'; 
+        clearInterval(modalTimerId); // if user opens modal himself don't open modal in few seconds 
+    }
+
+    function closeModal() {
+        // modal.classList.toggle('show');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+    //SHOW MODAL in few seconds
+    const modalTimerId = setTimeout(openModal, 10000);
+
+    // SCROLL to down of page
+    function showModalByScroll() {
+        if (Math.round(window.pageYOffset) + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+    window.addEventListener('scroll', showModalByScroll);
+
 });
