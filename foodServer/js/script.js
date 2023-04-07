a3f1cd9811d322d75a4d651432c4e00df33d9ad2
Anastasiia <monkeyPink1313@outlook.com>
2023-04-07 14:46:43 +0300

    food server: RegExp

--- foodServer/js/script.js
+++ foodServer/js/script.js
@@ -364,11 +364,15 @@ window.addEventListener('DOMContentLoaded', () => {
        dotsArr.push(dot);
     }
 
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
+
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length -1)) {
+        if (offset == deleteNotDigits(width) * (slides.length -1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
 
         sliderInner.style.transform = `translateX(-${offset}px)`;
@@ -385,9 +389,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length -1);
+            offset = deleteNotDigits(width) * (slides.length -1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
 
         sliderInner.style.transform = `translateX(-${offset}px)`;
@@ -406,7 +410,7 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo -1);
+            offset = deleteNotDigits(width) * (slideTo -1);
 
             sliderInner.style.transform = `translateX(-${offset}px)`;
 
